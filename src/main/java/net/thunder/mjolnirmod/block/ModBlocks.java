5d1314781339432e40306ce54c1e7a2b4b250d5c
Thor Sammer <thesammmerhammer7@gmail.com>
2022-07-08 09:55:39 -0500

    Added the zircon lamp including recipes and loot tables

--- src/main/java/net/thunder/mjolnirmod/block/ModBlocks.java
+++ src/main/java/net/thunder/mjolnirmod/block/ModBlocks.java
@@ -14,6 +14,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.thunder.mjolnirmod.MjolnirMod;
 import net.thunder.mjolnirmod.block.custom.JumpyBlock;
+import net.thunder.mjolnirmod.block.custom.ZirconLampBlock;
 import net.thunder.mjolnirmod.item.ModCreativeModeTab;
 import net.thunder.mjolnirmod.item.ModItems;
 
@@ -46,6 +47,11 @@ public class ModBlocks {
     public static final RegistryObject<Block> JUMPY_BLOCK = registerBlock("jumpy_block",
             () -> new JumpyBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.ASGARD_TAB);
+
+    public static final RegistryObject<Block> ZIRCON_LAMP = registerBlock("zircon_lamp",
+            () -> new ZirconLampBlock(BlockBehaviour.Properties.of(Material.GLASS)
+                    .strength(6f).requiresCorrectToolForDrops().lightLevel(state -> state.getValue(ZirconLampBlock.LIT) ? 15 : 0)), ModCreativeModeTab.ASGARD_TAB);
+
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
